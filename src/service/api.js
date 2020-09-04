e4f5393b79663d0c0f3bc6ec0d89b50e3b791d4d
sfilhu <sfilhu@gmail.com>
2020-09-04 17:50:54 -0300

    ignore .env

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_ENDPOINT = process.env.API || 'http://localhost:3000/';
+const API_ENDPOINT = process.env.VUE_APP_API || 'http://localhost:3000/';
 
 const api = axios.create({
   baseURL: API_ENDPOINT,
